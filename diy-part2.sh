c44f0885a26f821d56ad2da67f1022c1f7ee7b50
cjz540693-cmyk <cjz540693@gmail.com>
2026-04-25 15:27:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 设置默认IP
 mkdir -p package/base-files/files/etc/uci-defaults
 
+# 默认IP
 cat > package/base-files/files/etc/uci-defaults/99-ip << 'EOF'
 #!/bin/sh
 uci set network.lan.ipaddr='192.168.110.1'
@@ -10,7 +10,7 @@ uci commit network
 exit 0
 EOF
 
-# 设置默认主题（argon）
+# 默认主题
 cat > package/base-files/files/etc/uci-defaults/30-theme << 'EOF'
 #!/bin/sh
 uci set luci.main.mediaurlbase='/luci-static/argon'

e9a144f7190f4cf578e3648d2523988df59870bf
cjz540693-cmyk <cjz540693@gmail.com>
2026-04-25 10:59:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.110.1/g' package/base-files/files/bin/config_generate
+# 设置默认IP
+mkdir -p package/base-files/files/etc/uci-defaults
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+cat > package/base-files/files/etc/uci-defaults/99-ip << 'EOF'
+#!/bin/sh
+uci set network.lan.ipaddr='192.168.110.1'
+uci commit network
+exit 0
+EOF
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 设置默认主题（argon）
+cat > package/base-files/files/etc/uci-defaults/30-theme << 'EOF'
+#!/bin/sh
+uci set luci.main.mediaurlbase='/luci-static/argon'
+uci commit luci
+exit 0
+EOF
+
+chmod +x package/base-files/files/etc/uci-defaults/*

e03cdf4465a0bd36f59e13521adc4a41d9c3b57c
cjz540693-cmyk <cjz540693@gmail.com>
2026-05-08 17:58:32 +0800

    Delete package/luci-app-tailscale directory

--- package/luci-app-tailscale/root/usr/lib/lua/luci/controller/tailscale.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.tailscale", package.seeall)
-
-function index()
-    if not nixio.fs.access("/usr/bin/tailscale") then
-        return
-    end
-
-    entry({"admin", "services", "tailscale"}, template("tailscale"), _("Tailscale"), 90)
-end
